d178149862b53e27a490b95353f3bb3013a1a006
abhijitsk <46513397+abhijitsk@users.noreply.github.com>
2020-12-28 10:29:03 +0000

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "0KNplHp7ZAPcSP8hJBbFzMUeu"
-consumer_secret = "nJFATXys4wL4PcigCfzlc6C5nWTY7IljDFs7ZixKPepBmrJur2"
-access_token_key = "99162393-jl6rOgbcTQ1mO3AJCtjafWC87Sr8rQeyhN2z2OpXM"
-access_token_secret = "qyxJzAKpEaukzL7xa3Lsnbc1SfhX5lrLMFf06zp9Ctk7T"
