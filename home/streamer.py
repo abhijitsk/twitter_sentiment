83c2fa1fe168bc215d2035aa3412328283771b81
unknown <abhijit.cusat@gmail.com>
2021-01-08 17:40:50 +0000

    Full text of tweet still has some errors and needed to be fixed.

--- home/streamer.py
+++ home/streamer.py
@@ -13,21 +13,25 @@ api = tweepy.API(auth)
 Todays_data = []
 
 class MyStreamListener(tweepy.StreamListener):
-    def on_status(self,data):
+    def on_status(self,status):
         try:
             client = MongoClient(MONGO_HOST)
             db = client.twitter
-            db.twitter_data1.insert_one(json.loads(data))
+            # if status.retweeted:  ### to get the original tweets 
+            db.twitter_text.insert_one(status._json)
+            print (status.truncated)
 
-        except:
-            pass
+        except :
+            print("Error in Status retrieval")
+            return False # to end the stream 
 
     def on_error(self,data):
         print('error 2')
+        
 
 
 if __name__ == "__main__":
     myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, )
     myStream.filter(track=['corona','COVID19'],languages = ['en'])
 
