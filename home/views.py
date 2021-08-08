593ae786c9b564103a55e3575bcaf71f24df7829
Abhijit sasikumar <abhijit.cusat@gmail.com>
2021-08-08 19:37:19 +0100

    pls

--- home/views.py
+++ home/views.py
@@ -9,10 +9,6 @@ from .apps import HomeConfig
 from rest_framework.views import APIView
 from django.http import JsonResponse
 
-consumer_key = "0KNplHp7ZAPcSP8hJBbFzMUeu"
-consumer_secret = "nJFATXys4wL4PcigCfzlc6C5nWTY7IljDFs7ZixKPepBmrJur2"
-access_token_key = "99162393-jl6rOgbcTQ1mO3AJCtjafWC87Sr8rQeyhN2z2OpXM"
-access_token_secret = "qyxJzAKpEaukzL7xa3Lsnbc1SfhX5lrLMFf06zp9Ctk7T"
 
 
 #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
