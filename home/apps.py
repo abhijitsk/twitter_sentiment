abb565838e377df9650462c679fca20fa87e4abd
abhijitsk <46513397+abhijitsk@users.noreply.github.com>
2021-08-30 23:51:51 +0100

    updates on server

--- home/apps.py
+++ home/apps.py
@@ -11,19 +11,20 @@ from keras.models import load_model
 class HomeConfig(AppConfig):
 
     name = 'dbMongo'
-    path = os.path.join(settings.MODELS, 'models1.p')
+    path = '/root/project_sentiment/twitter_sentiment/home/'
+    path3 = os.path.join(settings.MODELS, 'models1.p')
     path1 = os.path.join(settings.MODELS, 'modelsalla.p')
     path2 = os.path.join(settings.MODELS, 'LSTM.h5')
 
     
     
-    with open(path, 'rb') as pickled:
+    with open(path + 'models1.p', 'rb') as pickled:
         data = pickle.load(pickled)
 
-    with open(path1, 'rb') as pickled:
+    with open(path + 'modelsalla.p', 'rb') as pickled:
         token_LSTM = pickle.load(pickled)
 
-    model_LSTM = load_model(path2)
+    model_LSTM = load_model(path + 'LSTM.h5)
     
 
 
